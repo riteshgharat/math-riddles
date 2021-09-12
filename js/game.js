7f8881a02eaef788267788b7a9c51c8ec1de71f6
Ritesh Gharat <75104422+imritpro@users.noreply.github.com>
2021-09-12 12:56:50 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var input = document.querySelector('#input');
+ var input = document.querySelector('#input');
 var output = document.querySelector('.sum');
 var btnCheck = document.querySelector('#check');
 var subTitle = document.querySelector('.sub-title');
@@ -21,104 +21,14 @@ let state = {
 let minNo;
 let maxNo;
 
+var localhighscore = {
+  easy: 0,
+  medium: 0,
+  hard: 0
+}
 
 /* to play btn audio */
 var btnaudio = new Audio();
-//var clockaudio = new Audio();
-var bgmusic = new Audio();
-/*
-window.onload = function() {
-  bgmusic.src = 'music/bg.mp3';
-  bgmusic.play();
-};
-function music() {
-  bgmusic.pause();
-}
-*/
-
-//buttton music function
-document.querySelectorAll('button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btnaudio.src = 'music/buttonpress.mp3';
-    btnaudio.play();
-  })
-});
-document.querySelectorAll('span').forEach(span => {
-  span.addEventListener('click', () => {
-    btnaudio.src = 'music/buttonpress.mp3';
-    btnaudio.play();
-  })
-});
-
-/*displaying main container*/
-var mainContainer = document.querySelector('.main-container')
-
-function Play() {
-  mainContainer.classList.add('main-con-none');
-  /*document.querySelector('.about-app').classList.add('about-app-display');*/
-}
-
-function level() {
-  mainContainer.classList.remove('main-con-none');
-  /* document.querySelector('.about-app').classList.add('about-app-display');*/
-}
-
-/* displaying level container*/
-var levelContainer = document.querySelector('.level-container')
-
-function easy() {
-  minNo = 1;
-  maxNo = 10;
-  state.type = 'Easy';
-  updateProblem()
-  levelContainer.classList.add('level-con-none');
-  restartGame();
-}
-
-function medium() {
-  minNo = 10;
-  maxNo = 20;
-  state.type = 'Medium';
-  updateProblem()
-  levelContainer.classList.add('level-con-none');
-  restartGame();
-}
-
-function hard() {
-  minNo = 20;
-  maxNo = 50;
-  state.type = 'Hard';
-  updateProblem()
-  levelContainer.classList.add('level-con-none');
-  restartGame()
-}
-
-/* back btn function */
-var confirmBox = document.querySelector('.confirm-box-layer');
-
-function back() {
-  //var exitConfirm = confirm('Are sure you want to exit?');
-  clearInterval(StartInterval);
-  confirmBox.classList.add('confirm-box-display');
-  gameContainer.classList.add('blurred');
-}
-//confirm box
-function no() {
-  //clearInterval(StartInterval)false;
-  //setInterval(timer, 1000);
-  confirmBox.classList.remove('confirm-box-display');
-  gameContainer.classList.remove('blurred');
-}
-
-function yes() {
-  confirmBox.classList.remove('confirm-box-display');
-  gameContainer.classList.remove('blurred');
-
-  levelContainer.classList.remove('level-con-none');
-  clearInterval(StartInterval);
-  time = 61;
-}
-
 /*Main game function*/
 
 /*generating random number*/
@@ -154,14 +64,11 @@ function answerGen() {
   if (p.operation == "÷") answer = p.numOne / p.numTwo;
 
   /*finding weather ans is positive or negative and updaying problem*/
-  if (answer < 0) {
+  if (answer <= 5) {
     updateProblem();
-    //console.log('answer is negative')
-    //console.log(answer);
-  }
-  if (parseInt(answer) !== answer) {
-    updateProblem();
-    //console.log('answer is interger')
+    if (parseInt(answer) !== answer) {
+      updateProblem();
+    }
   }
 }
 
@@ -180,13 +87,12 @@ btnCheck.addEventListener('click', () => {
     }
     else {
       subTitle.innerHTML = 'Wrong Answer!'
-      state.wrongScore -= 5;
+      state.wrongScore -= 15;
       pointScore.innerHTML = state.score + state.wrongScore;
       state.wrongAns++;
       updateProblem();
 
       if (pointScore.textContent < 0) {
-        //subTitle.innerHTML = 'Wrong Answer!'
         state.score = 0
         state.wrongScore = 0;
         pointScore.innerHTML = 0;
@@ -204,30 +110,38 @@ let time;
 function timer() {
   time--;
   countdown.innerHTML = `${time}`;
-  /*clockaudio.src = 'music/clockMusic.mp3';
-  clockaudio.play();*/
   if (time <= 10) {
     countdown.classList.add('blink');
   } else {
     countdown.classList.remove('blink');
-    /* clockaudio.src = 'music/clockMusic.mp3';
-     clockaudio.pause();*/
   }
   if (time == 0) {
     clearInterval(StartInterval);
     time *= 0;
     subTitle.innerHTML = 'Time Up';
 
-    //playing audio
-
     overlay.classList.add('overlay-visible');
     gameContainer.classList.add('blurred');
     type.innerHTML = 'Game Over!' + '<br><br>' + `${state.type}` + ' Level';
-    endMessage.innerHTML = `${state.score + state.wrongScore}`;
-    // leadScore.innerHTML = `${state.type}: ${state.score + state.wrongScore}`;
+    var finalScore = `${state.score + state.wrongScore}`;
+    endMessage.innerHTML = finalScore;
 
-    // +'Right Ans: ' +`${state.rightAns}` + '<br>' +
-    // 'Wrong Ans: ' +`${state.wrongAns}` + '<br>';
+    /*Highscore board*/
+    if (finalScore > storedData.easy && state.type == 'Easy') {
+      localhighscore.easy = finalScore;
+      localStorage.setItem('localhighscore', JSON.stringify(localhighscore));
+      //  console.log('easy' + localhighscore + (finalScore > storedData.easy && state.type == 'Easy'))
+    }
+    else if (finalScore > storedData.medium && state.type == 'Medium') {
+      localhighscore.medium = finalScore;
+      localStorage.setItem('localhighscore', JSON.stringify(localhighscore));
+      //  console.log('medium' + localhighscore + (finalScore > storedData.medium && state.type == 'Medium'))
+    }
+    else if (finalScore > storedData.hard && state.type == 'Hard') {
+      localhighscore.hard = finalScore;
+      localStorage.setItem('localhighscore', JSON.stringify(localhighscore));
+      //   console.log('medium' + localhighscore + (finalScore > storedData.hard && state.type == 'Hard'))
+    }
   }
   else {
     subTitle.innerHTML = null;
@@ -236,7 +150,7 @@ function timer() {
 
 /*restart function*/
 function restartGame() {
-  state.score = 0
+  state.score = 0;
   state.wrongScore = 0;
   pointScore.innerHTML = 0;
   updateProblem();
